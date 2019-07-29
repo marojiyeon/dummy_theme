16b696e1ce420c47ab6907586613bd7392b404be
marojiyeon <marojiyeon@gmail.com>
2019-07-29 14:36:11 +0900

    First Commit

---
+++ custom/addons/dummy_theme/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    # Theme information
+    'name': "Dummy Theme",
+    'description': """
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates
+    'data': [
+        'views/options.xml',
+        'views/snippets.xml',
+        'views/layout.xml',
+        'views/pages.xml',
+        'views/assets.xml',
+    ],
+
+    # demo pages
+    'demo': [
+        #'demo/pages.xml',
+    ],
+
+    # Your information
+    'author': "My Company",
+    'website': "",
+}
